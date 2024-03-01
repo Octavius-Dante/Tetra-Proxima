66c0d276d6c0ef4fb926e18c0eeceef2684afac8
Lordes Dante Epicurus <vivi.lordes@gmail.com>
2024-02-29 21:17:18 -0800

    Add files via upload

---
+++ 02_Sample_Go_App/main.go
@@ -0,0 +1,24 @@
+package main
+
+import ( 
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// IndexHandler returns a simple message
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Dante test app BTP")
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+
+	var port string
+	if port = os.Getenv("PORT"); len(port) == 0 {
+	   port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
